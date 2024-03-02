fef7a80046ca7f4856ee0f9639adb2cd13f70857
biomlds <biomlds1@gmail.com>
2024-03-01 22:29:24 -0700

    update

--- setup.sh
+++ setup.sh
@@ -10,7 +10,11 @@ curl -sfLo ~/.local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/
 chmod a+x ~/.local/bin/yadm
 
 # Clone dotfiles repository
-~/.local/bin/yadm clone --bootstrap -f https://github.com/biomlds/.dotfiles.git
+#~/.local/bin/yadm clone --bootstrap -f https://github.com/biomlds/.dotfiles.git
+~/.local/bin/yadm clone  -f https://github.com/biomlds/.dotfiles.git
+~/.local/bin/yadm checkout /Users/$(whoami)
+~/.local/bin/yadm bootstrap
+
 
 # Clean up by removing yadm
 rm -rf ~/.local/bin/yadm
